6ff9459d3400ee613055d781cd960b6da0d7f63b
ABIPRASAD <abiprasad998@gmail.com>
2022-08-06 15:46:37 +0530

    with break

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
 #include<cstdio>
 using namespace std;
-void day();
+void day(int y);
 int main(){
- day(1);
+ int x=4;
+ day(x);
  return 0;
 }
 void day(int day){
   switch (day) {
-     case 1:cout<<"sunday";
-     case 2:cout<<"monday";
-     case 3:cout<<"tuesday";
-     case 4:cout<<"wednessday";
-     case 5:cout<<"thursday";
-     case 6:cout<<"friday";
-     default:cout<<"wrong input";
+     case 1:cout<<"sunday"<<endl;
+     case 2:cout<<"monday"<<endl;
+     case 3:cout<<"tuesday"<<endl;
+     case 4:cout<<"wednessday"<<endl;
+     case 5:cout<<"thursday"<<endl;
+     case 6:cout<<"friday"<<endl;
+     default:cout<<"wrong input"<<endl;
 
-  }
+}
 }
