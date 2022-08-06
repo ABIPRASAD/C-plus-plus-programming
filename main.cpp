4fd88cceae81c71ab8eaac19eec84dba74a1b7d4
ABIPRASAD <abiprasad998@gmail.com>
2022-08-06 15:36:54 +0530

    without break

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
+#include<cstdio>
 using namespace std;
+void day();
 int main(){
-//
-char name[10];
-int i;
-cout<<"enetr your name:";
-for(i=0;i<3;i++){
-  cin>>name[i];
+ day(1);
+ return 0;
 }
-for(i=0;i<3;i++){
-  cout<<name[i];
-}
+void day(int day){
+  switch (day) {
+     case 1:cout<<"sunday";
+     case 2:cout<<"monday";
+     case 3:cout<<"tuesday";
+     case 4:cout<<"wednessday";
+     case 5:cout<<"thursday";
+     case 6:cout<<"friday";
+     default:cout<<"wrong input";
+
+  }
 }
